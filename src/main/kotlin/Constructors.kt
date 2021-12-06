30ba996ed917c917982fbc2c949c4c01a80dde47
Vishal <1316510155@kit.ac.in>
2021-12-06 17:32:24 +0530

    4th commit

--- src/main/kotlin/Constructors.kt
+++ src/main/kotlin/Constructors.kt
@@ -1,9 +1,46 @@
-class Add constructor(val a:Int, val b:Int){
+class Add constructor(val a:Int, val b:Int) // primary constructor
+
+{
     var c= a+b
 
+}
+
+class Automobile(val name:String,val tyer:Int, val maxSeat:Int) // primary constructor
+{
+
+    //init block is used if we want to run some set of statement when we create an object then we use init block
+
+    init {
+        println("$name is created")
+    }
+
+    init {
+        println("Second init block")
+    }
+    fun drive(){
+
+    }
+
+    fun applyBreak(){
+
+
+    }
+}
+
+
+class Person1(nameParam:String,ageParam:Int){
+    val name =nameParam
+    val age = ageParam
 }
 
 fun main() {
     val add = Add(44,33)
     println("the sum of number ${add.c}")
+
+    val car =Automobile("Car",4,5)
+    println(" ${car.name}  --- ${car.tyer} --- ${car.maxSeat}")
+
+    var person1 = Person1("ravi",22)
+    println(person1.age)
+    println(person1.name)
 }
